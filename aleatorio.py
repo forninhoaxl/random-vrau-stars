0b6f0ee6ae9cdae9635ea77519559c775f8d1bb9
Rodrigo Gomes <rodrigogr1602@gmail.com>
2020-06-24 13:51:35 -0300

    v2 aleatorio

--- aleatorio.py
+++ aleatorio.py
@@ -1,7 +1,9 @@
 import random
 
 listaR = ["Sheila","Anita","Coito","Jessica","Indio","Pouco","Poper","Bull","Brock","Dinalixo","El primo","Rosa","Barril","Penys","Pançuda","Cachaçeiro","Pobre","Carlinhos","xbox","Cu de rolha","Frank","Chiclete fudido","Tarada","Spoke","Jacky","Emz","Bixa sombria","Abelha","NANI","Cabeça de gelo"]
-#listaA = []
+listaA = ['Rato', 'Frank', 'Pançuda', 'Jessie', 'Shelly', 'Carlos','Eugênio', 'SproutBaitola', 'Brock', 'Feminista', 'Bull','Chico', 'Dynamike', 'JailsonMendes', 'Colt', 'Nita','Poco', 'El-Primo', 'Barley', 'Rosa', 'Rico', 'RolaBosta','Penny', 'Piper', 'Bibi', 'Tara', 'SpikeInútil', 'Corvo','EMZ', 'Jacky', 'Bea', 'Max', 'Mr.P', 'Gale', 'Sandy']
 #listaE = []
 
 print("Inshan: " + random.choice(listaR))
+print("Forninho: " + random.choice(listaA))
+#print("Kaedus: " + random.choice(listaE))
