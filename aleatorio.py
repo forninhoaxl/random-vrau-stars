52a9e67b8e0bc679d11f9702f0f017cbf74dd040
forninhoaxl <67068681+forninhoaxl@users.noreply.github.com>
2020-06-24 14:20:44 -0300

    minha bilola

--- aleatorio.py
+++ aleatorio.py
@@ -1,7 +1,7 @@
 import random
 
 listaR = ["Sheila","Anita","Coito","Jessica","Indio","Pouco","Poper","Bulrro","Inutil","Dinalixo","Ricardo Milos","Maguila mulher","Barril","Penys","Pançuda","Cachaçeiro","Pobre","Carlinhos","Xbox mil grau","Cu de rolha","Guto bate com um martelo","Chiclete fudido","Tarada","Spoke","Furadeira","Pichadora","Bixa sombria","Abelha","NANI","Cabeça de gelo"]
-listaA = ['Rato', 'Frank', 'Pançuda', 'Jessie', 'Shelly', 'Carlos','Eugênio', 'SproutBaitola', 'Brock', 'Feminista', 'Bull','Chico', 'Dynamike', 'JailsonMendes', 'Colt', 'Nita','Poco', 'El-Primo', 'Barley', 'Rosa', 'Rico', 'RolaBosta','Penny', 'Piper', 'Bibi', 'Tara', 'SpikeInútil', 'Corvo','EMZ', 'Jacky', 'Bea', 'Max', 'Mr.P', 'Gale', 'Sandy']
+listaA = ['Rato', 'Frank', 'Pançuda', 'Jessie', 'Shelly', 'Carlos','Eugênio', 'SproutBaitola', 'Inútil', 'Pançuda', 'Bull','Chico', 'Dynamike', 'JailsonMendes', 'Colt', 'Nita','Poco', 'Ricardo Milos', 'Barley', 'Rosa', 'Rico', 'RolaBosta','Penny', 'Piper', 'Bibi', 'Tara', 'Mudim', 'Corvo','EMZ', 'Jacky', 'AbelhaBugada', 'EjaculaçãoPrecoce', 'Mr.P', 'Idoso', 'Sandy']
 #listaE = []
 
 print("Inshan: " + random.choice(listaR))
