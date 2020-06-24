0d1a2f23fc11cf62d7b273fb74ec9c1a64a8681d
Rodrigo Gomes <rodrigogr1602@gmail.com>
2020-06-24 13:45:19 -0300

    v1 aleatorio

---
+++ aleatorio.py
@@ -0,0 +1,7 @@
+import random
+
+listaR = ["Sheila","Anita","Coito","Jessica","Indio","Pouco","Poper","Bull","Brock","Dinalixo","El primo","Rosa","Barril","Penys","Pançuda","Cachaçeiro","Pobre","Carlinhos","xbox","Cu de rolha","Frank","Chiclete fudido","Tarada","Spoke","Jacky","Emz","Bixa sombria","Abelha","NANI","Cabeça de gelo"]
+#listaA = []
+#listaE = []
+
+print("Inshan: " + random.choice(listaR))
