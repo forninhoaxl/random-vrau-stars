adbef4dda683943cbbeebb33af8835b9ce9766b8
Rodrigo Gomes <rodrigogr1602@gmail.com>
2020-06-24 14:10:18 -0300

    v3 aleatorio (cada vez pior rsrs)

--- aleatorio.py
+++ aleatorio.py
@@ -1,6 +1,6 @@
 import random
 
-listaR = ["Sheila","Anita","Coito","Jessica","Indio","Pouco","Poper","Bull","Brock","Dinalixo","El primo","Rosa","Barril","Penys","Pançuda","Cachaçeiro","Pobre","Carlinhos","xbox","Cu de rolha","Frank","Chiclete fudido","Tarada","Spoke","Jacky","Emz","Bixa sombria","Abelha","NANI","Cabeça de gelo"]
+listaR = ["Sheila","Anita","Coito","Jessica","Indio","Pouco","Poper","Bulrro","Inutil","Dinalixo","Ricardo Milos","Maguila mulher","Barril","Penys","Pançuda","Cachaçeiro","Pobre","Carlinhos","Xbox mil grau","Cu de rolha","Guto bate com um martelo","Chiclete fudido","Tarada","Spoke","Furadeira","Pichadora","Bixa sombria","Abelha","NANI","Cabeça de gelo"]
 listaA = ['Rato', 'Frank', 'Pançuda', 'Jessie', 'Shelly', 'Carlos','Eugênio', 'SproutBaitola', 'Brock', 'Feminista', 'Bull','Chico', 'Dynamike', 'JailsonMendes', 'Colt', 'Nita','Poco', 'El-Primo', 'Barley', 'Rosa', 'Rico', 'RolaBosta','Penny', 'Piper', 'Bibi', 'Tara', 'SpikeInútil', 'Corvo','EMZ', 'Jacky', 'Bea', 'Max', 'Mr.P', 'Gale', 'Sandy']
 #listaE = []
 
