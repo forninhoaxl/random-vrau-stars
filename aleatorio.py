11477632e26ac161f0c30b9574345a616891f659
forninhoaxl <67068681+forninhoaxl@users.noreply.github.com>
2020-06-25 16:53:42 -0300

    meu pau de óculos
    eae men

--- aleatorio.py
+++ aleatorio.py
@@ -1,7 +1,7 @@
 import random
 
 listaR = ["Sheila","Anita","Coito","Jessica","Indio","Pouco","Poper","Bulrro","Inutil","Dinalixo","Ricardo Milos","Maguila mulher","Barril","Penys","Pançuda","Cachaçeiro","Pobre","Carlinhos","Xbox mil grau","Cu de rolha","Guto bate com um martelo","Chiclete fudido","Tarada","Spoke","Furadeira","Pichadora","Bixa sombria","Abelha","NANI","Cabeça de gelo"]
-listaA = ['Rato', 'Frank', 'Pançuda', 'Jessie', 'Shelly', 'Carlos','Eugênio', 'SproutBaitola', 'Inútil', 'Pançuda', 'Bull','Chico', 'Dynamike', 'JailsonMendes', 'Colt', 'Beicim','Poco', 'Ricardo Milos', 'Barley', 'Rosa', 'Rico', 'RolaBosta','Penny', 'Supositório', 'Bibi', 'Tara', 'Mudim', 'Corvo','EMZ', 'Jacky', 'AbelhaBugada', 'EjaculaçãoPrecoce', 'Mr.P', 'Idoso', 'Sandy']
+listaA = ['Sheila', 'Urso Peludo', 'Colt', 'Bull', 'Jessie', 'Inútil', 'Dynamike', 'Caucaia', 'Chico', 'Caixa', 'EMZ', 'Ricardo Milos', 'Barley','Poco', 'Rosa', 'Rico', 'RolaBosta','Penny',  'Carlos', 'Jacky', 'Supositório', 'Pançuda', 'Frank', 'Bibi', 'AbelhaBugada', 'Rato', 'Tara', 'Beicim', 'EjaculaçãoPrecoce', 'Mr.P', 'SproutBaitola', 'Mudim', 'Corvo', 'Sandy', 'Idoso',]
 listaE = ['Rato', 'Frank', 'Pançuda', 'Jessie', 'Shelly', 'Carlos', 'Eugênio', 'Brock', 'Feminista', 'Bull','Chico', 'Dynamike', 'JailsonMendes', 'Colt', 'Nita', 'Poco', 'El-Primo', 'Barley', 'Rosa', 'Rico', 'RolaBosta', 'Penny', 'Piper', 'Bibi', 'EMZ', 'Jacky', 'Bea', 'Mr.P', 'Gale',]
 
 
